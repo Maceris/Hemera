a061c48272d7dfc433559029eb40de379595c94e
David Burks <chesburks@gmail.com>
2025-03-11 00:28:55 -0400

    Update token struct

--- apps/bootstrap_compiler/include/lexer/token.h
+++ apps/bootstrap_compiler/include/lexer/token.h
@@ -1,7 +1,26 @@
 #pragma once
 
-namespace hemera {
-	enum Token {
+#include <cstdint>
 
+#include "memory/allocator.h"
+
+namespace hemera {
+
+	enum TokenType : uint16_t {
+
+	};
+
+	struct Token {
+		TokenType type;
+		uint16_t column_number;
+		uint32_t line_number;
+		MyString value;
+
+		Token() = default;
+		Token(const Token&) = default;
+		Token(Token&&) = default;
+		~Token() = default;
+		Token& operator=(const Token&) = default;
+		Token& operator=(Token&&) = default;
 	};
 }
