e9cf2a4cd6bf1ae0c1e97c6690668d1ab0a6b10b
David Burks <chesburks@gmail.com>
2025-04-17 19:45:27 -0400

    Add string literals

--- apps/bootstrap_compiler/src/lexer/lexer.cpp
+++ apps/bootstrap_compiler/src/lexer/lexer.cpp
@@ -570,8 +570,37 @@ namespace hemera::lexer {
 			}
 			break;
 		case State::literal_string:
+			switch (next_char(tokenizer, input_stream, token_contents)) {
+			case '"':
+				break;
+			case '\\':
+				tokenizer.state = State::literal_string_backslash;
+				break;
+			case 0:
+			case '\n':
+			case 0x01: case 0x02: case 0x03: case 0x04: case 0x05:
+			case 0x06: case 0x07: case 0x08: case 0x09: case 0x0b:
+			case 0x0c: case 0x0d: case 0x0e: case 0x0f: case 0x10:
+			case 0x11: case 0x12: case 0x13: case 0x14: case 0x15:
+			case 0x16: case 0x17: case 0x18: case 0x19: case 0x1a:
+			case 0x1b: case 0x1c: case 0x1d: case 0x1e: case 0x1f:
+			case 0x7f:
+				tokenizer.state = State::invalid;
+				goto switch_start;
+			default:
+				goto switch_start;
+			}
 			break;
 		case State::literal_string_backslash:
+			switch (next_char(tokenizer, input_stream, token_contents)) {
+			case 0:
+			case '\n':
+				tokenizer.state = State::invalid;
+				goto switch_start;
+			default:
+				tokenizer.state = State::literal_string;
+				goto switch_start;
+			}
 			break;
 		case State::minus:
 			break;
