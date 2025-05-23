05984a43a4292b60f3911b5414f1fe55c247e9d8
David Burks <chesburks@gmail.com>
2025-05-23 17:22:28 -0400

    Add in missing error messages for functions

--- apps/bootstrap_compiler/src/parser/parser.cpp
+++ apps/bootstrap_compiler/src/parser/parser.cpp
@@ -230,7 +230,6 @@ namespace hemera::parser {
 			return;
 		}
 		comments(state, parent);
-		//TODO(ches) type
 		type(state, parent);
 	}
 
@@ -270,7 +269,7 @@ namespace hemera::parser {
 	void generic_tag(ParserState* state, ast::Node& parent) {
 		ast::Node& node = next_as_node(state, ast::NodeType::GENERIC_TAG, parent);
 		if (!skip(state, TokenType::SYM_LT)) {
-			// TODO(ches) error??
+			report_error_on_last_token(state, ErrorCode::E3011);
 		}
 
 		while (!expect(state, TokenType::SYM_GT)) {
@@ -280,28 +279,27 @@ namespace hemera::parser {
 			}
 		}
 		if (!skip(state, TokenType::SYM_GT)) {
-			// TODO(ches) error??
+			report_error_on_last_token(state, ErrorCode::E3012);
 		}
 
 	}
 
 	void function_signature(ParserState* state, ast::Node& parent) {
-		if (state == nullptr) { parent.type; } //TODO(ches) remove this
 		ast::Node& node = next_as_node(state, ast::NodeType::FUNCTION, parent);
 		if (!accept(state, TokenType::KEYWORD_FN, node)) {
-			// TODO(ches) error??
+			report_error_on_last_token(state, ErrorCode::E3013);
 		}
 		if (expect(state, TokenType::SYM_LT)) {
 			generic_tag(state, node);
 		}
 		if (!skip(state, TokenType::SYM_LPAREN)) {
-			// TODO(ches) error??
+			report_error_on_last_token(state, ErrorCode::E3014);
 		}
 		if (expect(state, TokenType::IDENTIFIER)) {
 			function_parameter_list(state, node);
 		}
 		if (!skip(state, TokenType::SYM_RPAREN)) {
-			// TODO(ches) error??
+			report_error_on_last_token(state, ErrorCode::E3015);
 		}
 		if (expect(state, TokenType::SYM_ARROW_SINGLE)) {
 			function_output_list(state, node);
@@ -313,7 +311,7 @@ namespace hemera::parser {
 
 		while (accept(state, TokenType::IDENTIFIER, node)) {
 			if (!skip(state, TokenType::SYM_COLON)) {
-				// TODO(ches) error??
+				report_error_on_last_token(state, ErrorCode::E3016);
 			}
 			type(state, node);
 			if (expect(state, TokenType::OPERATOR_EQUAL)) {
