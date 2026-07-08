f65b3f62418ae2aefd6a42e6fd3b65bb5c70bee0
David Burks <chesburks@gmail.com>
2026-07-07 21:15:09 -0400

    Add dummy pass code

--- apps/compiler/mlir/lib/hmir/hmir_passes.cpp
+++ apps/compiler/mlir/lib/hmir/hmir_passes.cpp
@@ -9,5 +9,36 @@ namespace mlir::hmir {
 #define GEN_PASS_DEF_HEMERADEFER
 #include "hmir/hmir_passes.h.inc"
 
-	//TODO(ches) add pass for defer
-}
+namespace {
+    class HemeraDeferRewriter : public OpRewritePattern<::hmir::HemeraDeferOp> {
+    public:
+        using OpRewritePattern<::hmir::HemeraDeferOp>::OpRewritePattern;
+        
+        LogicalResult matchAndRewrite(::hmir::HemeraDeferOp op,
+            PatternRewriter& rewriter) const final
+        {
+            //TODO(ches) find where we need to defer to, and do so
+
+            // Some dummy code that should in theory unwrap the op in place
+            mlir::Value deferInput = op.getOperand(0);
+            rewriter.replaceOp(op, { deferInput });
+            return success();
+        }
+    };
+
+    class HemeraDefer
+        : public impl::HemeraDeferBase<HemeraDefer> {
+    public:
+        using impl::HemeraDeferBase<HemeraDefer>::HemeraDeferBase;
+        
+        void runOnOperation() final {
+            RewritePatternSet patterns(&getContext());
+            patterns.add<HemeraDeferRewriter>(&getContext());
+            FrozenRewritePatternSet patternSet(std::move(patterns));
+            if (failed(applyPatternsGreedily(getOperation(), patternSet))) {
+                signalPassFailure();
+            }
+        }
+    };
+} // namespace
+} // namespace mlir::hmir
